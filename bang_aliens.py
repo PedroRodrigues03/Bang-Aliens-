0283a7ea649964dfcc49a220337b5bed8eac8db9
Pedro Rodrigues <pedrolrodrigues3@gmail.com>
2023-11-13 16:53:14 -0300

    ship.py, bullet.py have been created, now we can shoot boolets up the screen

--- bang_aliens.py
+++ bang_aliens.py
@@ -7,6 +7,8 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+from bullet import Bullet
 
 class BangAliens:
     """Overall class to manage game assets and behavior."""
@@ -18,25 +20,81 @@ class BangAliens:
         pygame.init()
         self.settings = Settings()
 
-        # pygame.display.set_mode() creates a display window, on which we'll draw all the game's graphical elements. The argument (1200, 800) is a tuple that defines the dimensions of the game window, which will be 1200 pixels wide by 800 pixels hight. I assign this display window to the attribute self.screen, so it will be available in all methods in the class
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # pygame.display.set_mode() creates a display window, on which we'll draw all the game's graphical elements.
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Bang, Aliens!")
 
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     # The game is controlled by the run_game() method. This method contains a while loop that runs continually. The while loop contains an event loop and code that manages screen updates.
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
+            
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        # Respond to keypress and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyups_events(event)
+    
+    def _check_keydown_events(self, event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move the ship to the left
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyups_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        #Update bullet position
+        self.bullets.update()
+
+        #Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitem()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
---
+++ ship.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship"""
+
+    def __init__(self, ba_game):
+        """Initializate the ship and set its starting position"""
+        self.screen = ba_game.screen
+        self.settings = ba_game.settings
+        self.screen_rect = ba_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship2.png').convert()
+        self.image.set_colorkey((0, 0, 35))
+        self.rect = self.image.get_rect()
+
+        #Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #Store decimals value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement Flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flags"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #Update rect object from self.x
+        self.rect.x = self.x
+    
+    def blitem(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image, self.rect)
+
